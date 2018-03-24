219ce783d23ab5ed49f55d306c949cf5d410a3d0
matias erenchun <matiaserenchun@gmail.com>
2018-03-24 17:20:50 -0300

    primer Hbox
    prueba 1

--- EjemplosFX/src/ejemplosfx/EjemplosFX.java
+++ EjemplosFX/src/ejemplosfx/EjemplosFX.java
@@ -5,11 +5,14 @@
  */
 package ejemplosfx;
 
+import java.awt.Insets;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -33,13 +36,22 @@ public class EjemplosFX extends Application {
 
         });
         
+       HBox hbox = new HBox();
+       Button button1 = new Button("Add");
+       Button button2 = new Button("Remove");
+       HBox.setHgrow(button1, Priority.ALWAYS);
+       HBox.setHgrow(button2, Priority.ALWAYS);
+       button1.setMaxWidth(Double.MAX_VALUE);
+       button2.setMaxWidth(Double.MAX_VALUE);
+       hbox.getChildren().addAll(button1, button2,btnPrueba);
+        
         StackPane root = new StackPane();
         
         primaryStage.setTitle("wena men ");//titulo de la ventana 
         
         Scene scene = new Scene (root,300,300);//constructor de la ventana 
         
-        root.getChildren().add(btnPrueba);//agregamos el boton a al root
+        root.getChildren().add(hbox);//agregamos el boton a al root
         primaryStage.setScene(scene);//agregamos scene a la pantalla
         primaryStage.show();//mostramos la pantalla
     }
