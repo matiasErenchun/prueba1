67f26daa0adc2a72968badf83165a9681be78a5a
matias erenchun <matiaserenchun@gmail.com>
2018-03-24 19:22:39 -0300

    ahora todo dentro del border pane
    se agregan los botones al bot

--- EjemplosFX/src/ejemplosfx/EjemplosFX.java
+++ EjemplosFX/src/ejemplosfx/EjemplosFX.java
@@ -11,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
@@ -37,20 +38,23 @@ public class EjemplosFX extends Application {
 
         });
         
-       HBox hbox = new HBox(40);
-       hbox.setPadding(new Insets(10));
+       HBox hbox = new HBox(40);//creamos el Hbox
+       hbox.setPadding(new Insets(20));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
        Button button1 = new Button("Add");
        Button button2 = new Button("Remove");
-       HBox.setMargin(button1, new Insets(0,0,0,5));
-       HBox.setMargin(button2, new Insets(0,0,0,5));
-       HBox.setMargin(btnPrueba, new Insets(0,0,0,5));
-       HBox.setHgrow(button1, Priority.ALWAYS);
+       HBox.setMargin(button1, new Insets(0,0,0,0));//margen entre objetos en este caso se dejo 5 a la  derecha 
+       HBox.setMargin(button2, new Insets(0,0,0,0));
+       HBox.setMargin(btnPrueba, new Insets(0,0,0,0));
+       HBox.setHgrow(button1, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
        HBox.setHgrow(button2, Priority.ALWAYS);
        HBox.setHgrow(btnPrueba, Priority.ALWAYS);
        HBox.setMargin(button1, javafx.geometry.Insets.EMPTY);
        button1.setMaxWidth(Double.MAX_VALUE);
        button2.setMaxWidth(Double.MAX_VALUE);
        
+       
+       BorderPane BpanePrueba = new BorderPane();
+       BpanePrueba.setBottom(hbox);
        hbox.getChildren().addAll(button1, button2,btnPrueba);
         
         StackPane root = new StackPane();
@@ -59,7 +63,7 @@ public class EjemplosFX extends Application {
         
         Scene scene = new Scene (root,300,300);//constructor de la ventana 
         
-        root.getChildren().add(hbox);//agregamos el boton a al root
+        root.getChildren().add(BpanePrueba);//agregamos el boton a al root
         primaryStage.setScene(scene);//agregamos scene a la pantalla
         primaryStage.show();//mostramos la pantalla
     }
