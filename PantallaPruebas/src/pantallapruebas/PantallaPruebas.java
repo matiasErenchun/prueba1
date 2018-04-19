83e8649b18464ba70a2d00c4bd2b3b47b7511767
matias erenchun <matiaserenchun@gmail.com>
2018-04-19 18:47:26 -0300

    termine la forma de los numeros

--- PantallaPruebas/src/pantallapruebas/PantallaPruebas.java
+++ PantallaPruebas/src/pantallapruebas/PantallaPruebas.java
@@ -38,48 +38,26 @@ public class PantallaPruebas extends Application {
                 System.out.println("Hello World!");
             }
         });
-        int a=0;
+        int a=-50;
         Path miPath = new Path();
-        
-        MoveTo start6 = new MoveTo();
-        start6.setX(200);
-        start6.setY(125);
-        
-        QuadCurveTo arco1 = new QuadCurveTo();
-        arco1.setX(175);
-        arco1.setY(100);
-        arco1.setControlX(200);
-        arco1.setControlY(100);
-        
-        
-        QuadCurveTo arco2 = new QuadCurveTo();
-        arco2.setX(150);
-        arco2.setY(125);
-        arco2.setControlX(150);
-        arco2.setControlY(100);
-        
-        QuadCurveTo arco3 = new QuadCurveTo();
-        arco3.setX(175);
-        arco3.setY(150);
-        arco3.setControlX(150);
-        arco3.setControlY(150);
-        
-        QuadCurveTo arco4 = new QuadCurveTo();
-        arco4.setX(200);
-        arco4.setY(125);
-        arco4.setControlX(200);
-        arco4.setControlY(150);
-        
-        VLineTo lineaV = new VLineTo();
-        
-        lineaV.setY(200);
-        
-         miPath.getElements().addAll(start6,arco1,arco2,arco3,arco4,lineaV);
-        
+        this.dibujar8(a, 0, miPath);
+        a+=50;
+       this.dibujar3(a, 0, miPath);
+       a+=50;
+       this.dibujar6(a, 0, miPath);
+       a+=60;
+       this.dibujar2(a, 0, miPath);
+       a+=60;
+       this.dibujar5(a, 0, miPath);
+       a+=60;
+       this.dibujar7(a, 0, miPath);
+       a+=60;
+       this.dibujar9(a, 0, miPath);
+       
         Pane root = new Pane();
         root.getChildren().add(miPath);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 400, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
@@ -98,30 +76,30 @@ public class PantallaPruebas extends Application {
     public Path dibujar2(int x, int y,Path miPath)
     {
         MoveTo start2 = new MoveTo();
-        start2.setX(200);
-        start2.setY(200);
+        start2.setX(150+x);
+        start2.setY(125);
         
         HLineTo base = new HLineTo();
-        base.setX(150);
+        base.setX(200+x);
         
         LineTo diagonal = new LineTo();
-        diagonal.setX(200);
-        diagonal.setY(125);
+        diagonal.setX(150+x);
+        diagonal.setY(200);
         
         QuadCurveTo primerArco = new QuadCurveTo();
-        primerArco.setX(175);
+        primerArco.setX(175+x);
         primerArco.setY(100);
-        primerArco.setControlX(200);
+        primerArco.setControlX(150+x);
         primerArco.setControlY(100);
         
         
         QuadCurveTo segundoArco = new QuadCurveTo();
-        segundoArco.setX(150);
+        segundoArco.setX(200+x);
         segundoArco.setY(125);
-        segundoArco.setControlX(150);
+        segundoArco.setControlX(200+x);
         segundoArco.setControlY(100);
         
-         miPath.getElements().addAll(start2,base,diagonal,primerArco,segundoArco);
+         miPath.getElements().addAll(start2,primerArco,segundoArco,diagonal,base);
         
         return miPath;
     }
@@ -130,40 +108,40 @@ public class PantallaPruebas extends Application {
     {
         // revisar la cabeza del 3 se podria hacer sus curbas mas pequeñas y subir el punto central para que quedara mejor 
         MoveTo start3 = new MoveTo();
-        start3.setX(160);
+        start3.setX(150+x);
         start3.setY(200);
         
         HLineTo base = new HLineTo();
-        base.setX(170);
+        base.setX(160+x);
         
         QuadCurveTo arco1 = new QuadCurveTo();
-        arco1.setX(200);
+        arco1.setX(190+x);
         arco1.setY(175);
-        arco1.setControlX(200);
+        arco1.setControlX(190+x);
         arco1.setControlY(200);
         
         
         QuadCurveTo arco2 = new QuadCurveTo();
-        arco2.setX(170);
+        arco2.setX(160+x);
         arco2.setY(150);
-        arco2.setControlX(200);
+        arco2.setControlX(190+x);
         arco2.setControlY(150);
         
         QuadCurveTo arco3 = new QuadCurveTo();
-        arco3.setX(200);
+        arco3.setX(190+x);
         arco3.setY(125);
-        arco3.setControlX(200);
+        arco3.setControlX(190+x);
         arco3.setControlY(150);
         
         
         QuadCurveTo arco4 = new QuadCurveTo();
-        arco4.setX(170);
+        arco4.setX(160+x);
         arco4.setY(100);
-        arco4.setControlX(200);
+        arco4.setControlX(190+x);
         arco4.setControlY(100);
         
         HLineTo htop = new HLineTo();
-        htop.setX(160);
+        htop.setX(150+x);
         
         
         
@@ -175,40 +153,40 @@ public class PantallaPruebas extends Application {
     public Path dibujar5(int x, int y , Path miPath )
     {
         MoveTo start5 = new MoveTo();
-        start5.setX(150);
+        start5.setX(150+x);
         start5.setY(190);
         
         QuadCurveTo arco1 = new QuadCurveTo();
-        arco1.setX(160);
+        arco1.setX(160+x);
         arco1.setY(200);
-        arco1.setControlX(150);
+        arco1.setControlX(150+x);
         arco1.setControlY(200);
         
         HLineTo base = new HLineTo();
-        base.setX(180);
+        base.setX(180+x);
         
         QuadCurveTo arco2 = new QuadCurveTo();
-        arco2.setX(200);
+        arco2.setX(200+x);
         arco2.setY(170);
-        arco2.setControlX(200);
+        arco2.setControlX(200+x);
         arco2.setControlY(200);
         
         QuadCurveTo arco3 = new QuadCurveTo();
-        arco3.setX(180);
+        arco3.setX(180+x);
         arco3.setY(140);
-        arco3.setControlX(200);
+        arco3.setControlX(200+x);
         arco3.setControlY(140);
         
         
         
         HLineTo hmid = new HLineTo();
-        hmid.setX(150);
+        hmid.setX(150+x);
         
         VLineTo vline = new VLineTo();
         vline.setY(100);
         
         HLineTo htop = new HLineTo();
-        htop.setX(200);
+        htop.setX(200+x);
         
          miPath.getElements().addAll(start5,arco1,base,arco2,arco3,hmid,vline,htop);
          
@@ -218,38 +196,38 @@ public class PantallaPruebas extends Application {
     public Path dibujar6(int x, int y , Path miPath)
     {
         MoveTo start6 = new MoveTo();
-        start6.setX(150);
+        start6.setX(150+x);
         start6.setY(175);
         
         QuadCurveTo arco1 = new QuadCurveTo();
-        arco1.setX(175);
+        arco1.setX(175+x);
         arco1.setY(200);
-        arco1.setControlX(150);
+        arco1.setControlX(150+x);
         arco1.setControlY(200);
         
         
         QuadCurveTo arco2 = new QuadCurveTo();
-        arco2.setX(200);
+        arco2.setX(200+x);
         arco2.setY(175);
-        arco2.setControlX(200);
+        arco2.setControlX(200+x);
         arco2.setControlY(200);
         
         QuadCurveTo arco3 = new QuadCurveTo();
-        arco3.setX(175);
+        arco3.setX(175+x);
         arco3.setY(150);
-        arco3.setControlX(200);
+        arco3.setControlX(200+x);
         arco3.setControlY(150);
         
         QuadCurveTo arco4 = new QuadCurveTo();
-        arco4.setX(150);
+        arco4.setX(150+x);
         arco4.setY(175);
-        arco4.setControlX(150);
+        arco4.setControlX(150+x);
         arco4.setControlY(150);
         
         QuadCurveTo arco5 = new QuadCurveTo();
-        arco5.setX(180);
+        arco5.setX(180+x);
         arco5.setY(100);
-        arco5.setControlX(155);
+        arco5.setControlX(155+x);
         arco5.setControlY(110);
         
          miPath.getElements().addAll(start6,arco1,arco2,arco3,arco4,arco5);
@@ -257,42 +235,153 @@ public class PantallaPruebas extends Application {
         return miPath;
     }
     
-    public Path pintar9(int x, int y , Path miPath)
+    public Path dibujar9(int x, int y , Path miPath)
     {
-        MoveTo start6 = new MoveTo();
-        start6.setX(200);
-        start6.setY(125);
+        MoveTo start9 = new MoveTo();
+        start9.setX(150+x);
+        start9.setY(125);
         
         QuadCurveTo arco1 = new QuadCurveTo();
-        arco1.setX(175);
-        arco1.setY(100);
-        arco1.setControlX(200);
-        arco1.setControlY(100);
+        arco1.setX(175+x);
+        arco1.setY(150);
+        arco1.setControlX(150+x);
+        arco1.setControlY(150);
         
         
         QuadCurveTo arco2 = new QuadCurveTo();
-        arco2.setX(150);
+        arco2.setX(200+x);
         arco2.setY(125);
-        arco2.setControlX(150);
-        arco2.setControlY(100);
-        
-        QuadCurveTo arco3 = new QuadCurveTo();
-        arco3.setX(175);
-        arco3.setY(150);
-        arco3.setControlX(150);
-        arco3.setControlY(150);
-        
-        QuadCurveTo arco4 = new QuadCurveTo();
-        arco4.setX(200);
-        arco4.setY(125);
-        arco4.setControlX(200);
-        arco4.setControlY(150);
+        arco2.setControlX(200+x);
+        arco2.setControlY(150);
         
         VLineTo lineaV = new VLineTo();
-        
         lineaV.setY(200);
         
-         miPath.getElements().addAll(start6,arco1,arco2,arco3,arco4,lineaV);
+        VLineTo lineaV2 = new VLineTo();
+        lineaV2.setY(125);
+        
+        QuadCurveTo arco3 = new QuadCurveTo();
+        arco3.setX(175+x);
+        arco3.setY(100);
+        arco3.setControlX(200+x);
+        arco3.setControlY(100);
+        
+        QuadCurveTo arco4 = new QuadCurveTo();
+        arco4.setX(150+x);
+        arco4.setY(125);
+        arco4.setControlX(150+x);
+        arco4.setControlY(100);
+        
+        
+        
+         miPath.getElements().addAll(start9,arco1,arco2,lineaV,lineaV2,arco3,arco4);
+        return miPath;
+    }
+    
+    public Path dibujar7(int x,int y,Path miPath)
+    {
+        MoveTo start7 = new MoveTo();
+        start7.setX(150+x);
+        start7.setY(200);
+        
+        LineTo diagonal1 = new LineTo();
+        diagonal1.setX(175+x);
+        diagonal1.setY(150);
+        
+        HLineTo hMidLine1 = new HLineTo();
+        hMidLine1.setX(200+x);
+        
+        HLineTo hMidLine2 = new HLineTo();
+        hMidLine2.setX(150+x);
+        
+        HLineTo hMidLine3 = new HLineTo();
+        hMidLine3.setX(175+x);
+        
+        LineTo diagonal2 = new LineTo();
+        diagonal2.setX(200+x);
+        diagonal2.setY(100);
+        
+        HLineTo lineaTop = new HLineTo();
+        lineaTop.setX(150+x);
+        
+         miPath.getElements().addAll(start7,diagonal1,hMidLine1,hMidLine2,hMidLine3,diagonal2,lineaTop);
+        
+        return miPath;
+    }
+    
+    public Path dibujar8(int x,int y, Path miPath)
+    {
+        MoveTo start8 = new MoveTo();
+        start8.setX(150+x);
+        start8.setY(175);
+        
+        QuadCurveTo arco1 = new QuadCurveTo();
+        arco1.setX(170+x);
+        arco1.setY(200);
+        arco1.setControlX(150+x);
+        arco1.setControlY(200);
+        
+        LineTo base = new LineTo();
+        base.setX(180+x);
+        base.setY(200);
+
+        QuadCurveTo arco2 = new QuadCurveTo();
+        arco2.setX(200+x);
+        arco2.setY(175);
+        arco2.setControlX(200+x);
+        arco2.setControlY(200);
+        
+        QuadCurveTo arco3 = new QuadCurveTo();
+        arco3.setX(180+x);
+        arco3.setY(150);
+        arco3.setControlX(200+x);
+        arco3.setControlY(150);
+        
+        LineTo mid1 = new LineTo();
+        mid1.setX(170+x);
+        mid1.setY(150);
+
+        QuadCurveTo arco4 = new QuadCurveTo();
+        arco4.setX(150+x);
+        arco4.setY(125);
+        arco4.setControlX(150+x);
+        arco4.setControlY(150);
+        
+        QuadCurveTo arco5 = new QuadCurveTo();
+        arco5.setX(170+x);
+        arco5.setY(100);
+        arco5.setControlX(150+x);
+        arco5.setControlY(100);
+        
+        LineTo htop = new LineTo();
+        htop.setX(180+x);
+        htop.setY(100);
+        
+        QuadCurveTo arco6 = new QuadCurveTo();
+        arco6.setX(200+x);
+        arco6.setY(125);
+        arco6.setControlX(200+x);
+        arco6.setControlY(100);
+        
+        QuadCurveTo arco7 = new QuadCurveTo();
+        arco7.setX(180+x);
+        arco7.setY(150);
+        arco7.setControlX(200+x);
+        arco7.setControlY(150);
+        
+        
+        LineTo mid2 = new LineTo();
+        mid2.setX(170+x);
+        mid2.setY(150);
+        
+        
+        
+        QuadCurveTo arco8 = new QuadCurveTo();
+        arco8.setX(150+x);
+        arco8.setY(175);
+        arco8.setControlX(150+x);
+        arco8.setControlY(150);
+        miPath.getElements().addAll(start8,arco1,base,arco2,arco3,mid1,arco4,arco5,htop,arco6,arco7,mid2,arco8);
         return miPath;
     }
     
