c349b9cfff33776ede9cd9dfea1b3204a2da9c26
matias erenchun <matiaserenchun@gmail.com>
2018-04-20 22:29:17 -0300

    intento de clases1
    esto es mi forma de como tendria que ser la  calculadora  aun es solo una prueba pero si funciona sera el infierno ajajajajajajajajajajaajj XD

---
+++ calculadora/src/calculadora/Dibujo.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.util.ArrayList;
+import javafx.scene.shape.PathElement;
+
+/**
+ *
+ * @author Matias.Erenchun
+ */
+public abstract class Dibujo 
+{
+    public Double topPoint;
+    public Double dawnPoint;
+    public Double startPoint;
+    public Double endPoint;
+    private ArrayList<ControlPoint> listControlPoint;
+    private ArrayList<PathElement> listCurves;
+
+    public Dibujo() 
+    {
+        this.listControlPoint = new ArrayList<>();
+        this.listCurves = new ArrayList<>();
+    }
+
+    public Double getTopPoint() 
+    {
+        return this.topPoint;
+    }
+
+    public void setTopPoint(Double topPoint) 
+    {
+        this.topPoint = topPoint;
+    }
+
+    public Double getDawnPoint() 
+    {
+        return this.dawnPoint;
+    }
+
+    public void setDawnPoint(Double dawnPoint) 
+    {
+        this.dawnPoint = dawnPoint;
+    }
+
+    public Double getStartPoint() 
+    {
+        return this.startPoint;
+    }
+
+    public void setStartPoint(Double startPoint) 
+    {
+        this.startPoint = startPoint;
+    }
+
+    public Double getEndPoint() 
+    {
+        return this.endPoint;
+    }
+
+    public void setEndPoint(Double endPoint) 
+    {
+        this.endPoint = endPoint;
+    }
+    
+    public int longListControlPoint()
+    {
+        return this.listControlPoint.size();
+    }
+    
+    public int longListCurves()
+    {
+        return this.listCurves.size();
+    }
+   
+    public ControlPoint getControlPointList(int index)
+    {
+       return this.listControlPoint.get(index);
+    }
+   
+    public PathElement getPathElementList(int index )
+    {
+        return this.listCurves.get(index);
+    }
+    
+    public ControlPoint removeControlPointList(int index)
+    {
+        return this.listControlPoint.remove(index);
+    }
+    
+    public PathElement removePathElementList(int index)
+    {
+        return this.listCurves.remove(index);
+    }
+    
+    public boolean isEmptyControlPointList()
+    {
+        return this.listControlPoint.isEmpty();
+    }
+    
+    public boolean isEmtyPathElementList()
+    {
+        return this.listCurves.isEmpty();
+    }
+}
