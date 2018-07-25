8df72a07d11c501bfc6bacae3a6e1e930ebf08c6
Unknown <mrrojano97@icloud.com>
2018-07-25 12:16:34 -0400

    CAMBIO CARPETA DE TRABAJO A proyecto_u3_v2_1_1_2_2
    Cambiar carpeta de trabajo a proyecto_u3_v2_1_1_2_2
    
    Nuevo método para mover números en división
    
    Arreglados puntos de control movidos

---
+++ proyecto_u3_v2_1_1_2_2/src/General/Main.java
@@ -0,0 +1,38 @@
+package General;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//modificado el 25 de abril a las 9:24
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Matias.Erenchun
+ * @author elias avendano
+ * @author mario Matamala
+ */
+
+public class Main extends Application {
+   
+    public Main() {
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pantalla pantalla=new Pantalla();
+    }
+    
+}
+
